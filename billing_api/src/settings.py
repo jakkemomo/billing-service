85d16ecf64dc7e68263dbd0d26d7ecf93242fa40
Oleg Smirnov <smirnov.nameless@ya.ru>
2021-05-25 00:18:52 +0300

    Add logging config

--- billing_api/src/settings.py
+++ billing_api/src/settings.py
@@ -1,17 +1,19 @@
 import logging
 from os import environ as env
+
+from core.logging import LOGGING_CFG
 from dotenv import load_dotenv
 
 load_dotenv()
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-DB_HOST: str = env.get("DB_HOST", "billing_db")
+logging.config.dictConfig(LOGGING_CFG)
+
+DB_HOST: str = env.get("DB_HOST", "localhost")
 DB_PORT: str = env.get("DB_PORT", "5432")
-DB_USER: str = env.get("DB_USER", "jaqombo")
-DB_PASSWORD: str = env.get("DB_PASSWORD", "12345")
+DB_USER: str = env.get("DB_USER", "billingapi")
+DB_PASSWORD: str = env.get("DB_PASSWORD", "billingapi")
 DB_NAME: str = env.get("DB_NAME", "billing")
-DB_SCHEMA: str = env.get("DB_SCHEMA", "data")
+DB_SCHEMA: str = env.get("DB_SCHEMA", "content")
 STRIPE_API_KEY = "sk_test_51InhtFIopSoE9boMy4b9JNDwgInO7S5xDpqsW1A1kL3SixGsw5EcGBG75TqpG3uTUDrpuA5OEPpXJeJBXTSlkO6d00WUws4eqc"
 
 TORTOISE_CFG = {
@@ -30,7 +32,7 @@ TORTOISE_CFG = {
     },
     "apps": {
         "billing": {
-            "models": ["src.orm.models", "src.aerich.models"],
+            "models": ["src.orm.models", "aerich.models"],
         }
     },
     "use_tz": True,
