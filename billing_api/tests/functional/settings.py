03cf9081c43f35d61c426336fb720788b8f4c322
jaqombo <alexolikov@gmail.com>
2021-05-29 16:20:11 +0300

    Remove bytes typing

--- billing_api/tests/functional/settings.py
+++ billing_api/tests/functional/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 class TestSettings(BaseSettings):
     STRIPE_API_KEY: str = os.environ.get("STRIPE_API_KEY", "test-api-key")
     DEBUG_USER_ID: str = "d306f620-2083-4c55-b66f-7171fffecc2b"
-    ACCESS_TOKEN: bytes = jwt.encode(
+    ACCESS_TOKEN = jwt.encode(
         headers={"alg": "HS256", "typ": "JWT"},
         payload={"sub": DEBUG_USER_ID, "iat": 1516239022},
         key="private-sign",
