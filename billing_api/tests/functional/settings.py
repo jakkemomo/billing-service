24faadff749a036ee64e1a7680ba386bbd3069fc
jaqombo <alexolikov@gmail.com>
2021-05-29 04:09:15 +0300

    Change environment variables in workflow

--- billing_api/tests/functional/settings.py
+++ billing_api/tests/functional/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class TestSettings(BaseSettings):
-    STRIPE_API_KEY: str = Field('check', env="STRIPE_API_KEY")
+    STRIPE_API_KEY: str = Field('check', env="STRIPE_TEST_API_KEY")
     ACCESS_TOKEN: str = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkMzA2ZjYyMC0yMDgzLTRjNTUtYjY2Zi03MTcxZmZmZWNjMmIiLCJpYXQiOjE1MTYyMzkwMjJ9.sf4fYKlDrLwtt55dvC_FKy5_MRLnCeUTOCCG723pNIs"
     DEBUG_USER_ID: str = "d306f620-2083-4c55-b66f-7171fffecc2b"
 
