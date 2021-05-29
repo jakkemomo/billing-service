98a7b919def64287453a80aa5f4791bb9d547664
jaqombo <alexolikov@gmail.com>
2021-05-29 03:29:43 +0300

    Remove env api key

--- billing_api/tests/functional/settings.py
+++ billing_api/tests/functional/settings.py
@@ -1,11 +1,6 @@
-from os import environ as env
-
-from dotenv import load_dotenv
 from src.core.settings import settings
 
-load_dotenv()
-
-STRIPE_API_KEY = env.get("STRIPE_API_KEY")
+STRIPE_API_KEY = settings.stripe.api_key
 STRIPE_URL = settings.stripe.url
 DEBUG_USER_ID = "d306f620-2083-4c55-b66f-7171fffecc2b"
 ACCESS_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkMzA2ZjYyMC0yMDgzLTRjNTUtYjY2Zi03MTcxZmZmZWNjMmIiLCJpYXQiOjE1MTYyMzkwMjJ9.sf4fYKlDrLwtt55dvC_FKy5_MRLnCeUTOCCG723pNIs"
