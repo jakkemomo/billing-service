af3bdafecd2fd12b8f37da7f444b1b16c49ff9fb
Oleg Smirnov <smirnov.nameless@ya.ru>
2021-05-30 12:38:07 +0300

    Add docstrings to db modules

--- billing_api/src/db/events.py
+++ billing_api/src/db/events.py
@@ -1,10 +1,19 @@
+"""Module with methods to init and release Tortoise ORM resources"""
+
 from tortoise import Tortoise
 
 
 async def tortoise_init(config: dict = None):
+    """
+    Init Tortoise ORM resources
+    @param config: Tortoise ORM config
+    """
     await Tortoise.init(config=config)
     await Tortoise.generate_schemas(safe=True)
 
 
 async def tortoise_release():
+    """
+    Release Tortoise ORM resources
+    """
     await Tortoise.close_connections()
